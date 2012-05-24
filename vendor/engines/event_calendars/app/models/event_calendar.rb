797eb0cb5a39dff8ace23740d5468c78188c8835
daudi amani <dsaronin@gmail.com>
2012-05-24 00:32:55 -0700

    drafing event_calendar - 2

--- vendor/engines/event_calendars/app/models/event_calendar.rb
+++ vendor/engines/event_calendars/app/models/event_calendar.rb
@@ -1,3 +1,18 @@
 class EventCalendar < ActiveRecord::Base
 
+   require 'rest-client'
+
+   def self.get_remote_events( url )
+
+      response = RestClient.get url
+
+      case response.code
+         when 200      then response
+         when 300..399 then "url incorrect; redirected"
+         when 400..499 then "error #{ response.code.to_s }"
+      else
+         "unknown error code: #{ response.code.to_s }"
+      end   # case
+
+   end
 end
