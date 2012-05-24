fedd57a0d92413f8a918bf26aef5c8564c454146
daudi amani <dsaronin@gmail.com>
2012-05-24 15:38:50 -0700

    drafting event calendar - 6

--- vendor/engines/event_calendars/app/controllers/event_calendars_controller.rb
+++ vendor/engines/event_calendars/app/controllers/event_calendars_controller.rb
@@ -3,6 +3,7 @@ class EventCalendarsController < ApplicationController
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @event_calendar in the line below:
+    @use_event_calendars = true
     present(@page)
   end
 
